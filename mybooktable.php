39273b390111e4eb6cc75b1a6ee281574be5e7c3
zookatron <zookatron@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-24 20:02:37 +0000

    MyBookTable 1.3.1
    git-svn-id: https://plugins.svn.wordpress.org/mybooktable/trunk@920457 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mybooktable.php
+++ mybooktable.php
@@ -5,10 +5,10 @@ Plugin URI: http://www.authormedia.com/mybooktable/
 Description: A WordPress Bookstore Plugin to help authors sell more books.
 Author: Author Media
 Author URI: http://www.authormedia.com
-Version: 1.3.0
+Version: 1.3.1
 */
 
-define("MBT_VERSION", "1.3.0");
+define("MBT_VERSION", "1.3.1");
 
 require_once("includes/functions.php");
 require_once("includes/setup.php");
@@ -56,7 +56,7 @@ function mbt_init() {
 	if($GLOBALS['pagenow'] == "plugins.php" and current_user_can('install_plugins') and isset($_GET['action']) and $_GET['action'] == 'deactivate' and isset($_GET['plugin']) and $_GET['plugin'] == plugin_basename(dirname(__FILE__)).'/mybooktable.php') { return; }
 	if($GLOBALS['pagenow'] == "plugins.php" and current_user_can('install_plugins') and isset($_GET['mbt_uninstall'])) { return mbt_uninstall(); }
 
-	if(function_exists('mbtdev_init')) { add_action('mbt_init', 'mbtdev_init'); } else if(function_exists('mbtpro_init')) { add_action('mbt_init', 'mbtpro_init'); }
+	if(function_exists('mbtdev_init') and (!defined('MBTPRO_VERSION') or (defined('MBTPRO_VERSION') and version_compare(MBTDEV_VERSION, MBTPRO_VERSION) >= 0))) { add_action('mbt_init', 'mbtdev_init'); } else if(function_exists('mbtpro_init')) { add_action('mbt_init', 'mbtpro_init'); }
 
 	do_action('mbt_before_init');
 
@@ -73,14 +73,22 @@ add_action('plugins_loaded', 'mbt_init');
 function mbt_rewrites_check() {
 	global $wp_rewrite;
 	$rules = $wp_rewrite->wp_rewrite_rules();
-	if(!isset($rules["books/?$"]) or $rules["books/?$"] !== "index.php?post_type=mbt_book") { flush_rewrite_rules(); }
+	if(!isset($rules[mbt_get_product_slug()."/?$"]) or $rules[mbt_get_product_slug()."/?$"] !== "index.php?post_type=mbt_book") { flush_rewrite_rules(); }
 }
 
 function mbt_customize_plugins_page() {
 	add_filter('plugin_action_links_'.plugin_basename(__FILE__), 'mbt_plugin_action_links');
+	add_filter('plugin_row_meta', 'mbt_plugin_row_meta', 10, 2);
 }
 
 function mbt_plugin_action_links($actions) {
 	$actions['settings'] = '<a href="'.admin_url('admin.php?page=mbt_settings').'">Settings</a>';
 	return $actions;
+}
+
+function mbt_plugin_row_meta($links, $file) {
+	if ($file == plugin_basename(__FILE__)) {
+		$links[] = '<a target="_blank" href="http://wordpress.org/support/view/plugin-reviews/mybooktable?filter=5#postform">' . __('Review this plugin', 'mybooktable') . '</a>';
+	}
+	return $links;
 }
